34256c5224ab7f9fd68f703f5362b706772ceba4
Dominic Matheron <d.matheron@gmail.com>
2011-11-13 13:52:07 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Engaccino::Application.config.secret_token = '4e44a18e2819491510a063e012695d6aaad93c763d66d4a944bbae7ae09652c66f40a321c8967882350d2a29211a90aa93b05a4a0366ac0fd688139c61797719'
