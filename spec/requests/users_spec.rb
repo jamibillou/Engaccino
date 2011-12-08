9a579b67e1093bb57619afbd7dec8ba513f85e83
Franck Sabattier <franck.sabattier@gmail.com>
2011-12-08 21:15:01 +0100

    Fixed the layout test, added a few standard users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -80,4 +80,40 @@ describe "Users" do
       end
     end
   end
+  
+  describe "visit users page" do
+    
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    describe "as an admin user" do
+      
+      before(:each) do
+        @user.toggle!(:admin)
+      end
+            
+      it "should have a destroy link" do
+        visit signin_path
+        fill_in :email,    :with => @user.email
+        fill_in :password, :with => @user.password
+        click_button
+        visit users_path
+        response.should have_selector("a", :href => user_path(@user), :id => "destroy_user_#{@user.id}")
+      end          
+    end
+  
+    describe "as a standard user" do
+      
+      it "should NOT have a destroy link" do
+        visit signin_path
+        fill_in :email,    :with => @user.email
+        fill_in :password, :with => @user.password
+        click_button
+        visit users_path
+        response.should_not have_selector("a", :href => user_path(@user), :id => "destroy_user_#{@user.id}")
+      end    
+    end    
+  end
+  
 end
