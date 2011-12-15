6ff34fe0e65aeb5520af9cd18a5f3c61e6fce667
Franck Sabattier <franck.sabattier@gmail.com>
2011-12-15 19:47:38 +0100

    Started to move user files to candidate

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,6 +1,18 @@
 class CandidatesController < ApplicationController
   
   def new
+    @candidates = Candidate.all
+    @title = t 'candidates.index.title'
+    @javascripts = ['users/index']
+  end
+  
+  def show
+    @candidate = Candidate.find(params[:id])
+    @title = "#{@candidate.first_name} #{@candidate.last_name}"
+  end
+  
+  def create
+    
   end
 
 end
--- spec/controllers/candidates_controller_spec.rb
+++ spec/controllers/candidates_controller_spec.rb
@@ -2,10 +2,80 @@ require 'spec_helper'
 
 describe CandidatesController do
 
+  render_views
+
+  before(:each) do
+    @candidate = Factory(:candidate)
+  end
+
+  describe "GET 'show'" do
+    
+    describe "for non-signed-in candidates" do
+      
+      it "should deny access to 'show'" do
+        get :show, :id => @candidate
+        response.should redirect_to(signin_path)
+        flash[:notice].should == I18n.t('flash.notice.please_signin')
+      end
+    end
+    
+    describe "for signed-in candidates" do
+    
+      before (:each) do
+        test_sign_in(@candidate)
+      end
+      
+      describe "who haven't completed signup" do
+        
+        it "should deny access to 'show'" do
+          get :show,  :id => @candidate
+          response.should redirect_to(edit_candidate_path(@candidate))
+          flash[:notice].should == I18n.t('flash.notice.please_finish_signup')
+        end
+      end
+      
+      describe "who have completed signup" do
+        
+        before(:each) do
+          @candidate.update_attributes(:profile_completion => 10)
+        end
+        
+        it "should return http success" do
+          get :show, :id => @candidate
+          response.should be_success
+        end
+        
+        it "should have the right selected navigation tab" do
+          get :show, :id => @candidate
+          response.should have_selector('li', :class => 'round selected', :content => I18n.t(:menu_profile))
+        end
+      end
+    end
+  end
+
   describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+  
+    describe "for signed-in users" do
+      
+      it "should deny access to 'new'" do
+        test_sign_in(@candidate)
+        get :new
+        response.should redirect_to(candidate_path(@candidate))
+        flash[:notice].should == I18n.t('flash.notice.already_registered')
+      end
+    end
+    
+    describe "for non-signed-in users" do
+    
+      it "should return http success" do
+        get :new
+        response.should be_success
+      end
+      
+      it "should have the right title" do 
+        get :new
+        response.should have_selector('title', :content => I18n.t('candidates.new.title'))
+      end
     end
   end
 
