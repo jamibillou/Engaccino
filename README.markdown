2b08612993f9a46ef0e34f79dd51b6e0c191fa84
Dominic Matheron <d.matheron@gmail.com>
2012-02-15 22:56:34 +0100

    Fixed cards for long job titles, organized translation files per class

--- README.markdown
+++ README.markdown
@@ -1,5 +1,6 @@
-#Engaccino
-##Recruiting and job hunting, socially.
+###Engaccino
+#####Recruiting and job hunting, socially.
+
 
 Developed by [Dominic Matheron](http://www.dominicmatheron.com) and [Franck Sabattier](http://www.linkedin.com/in/francksabattier).
 
--- app/helpers/professional_skill_candidates_helper.rb
+++ app/helpers/professional_skill_candidates_helper.rb
@@ -1,17 +1,17 @@
 module ProfessionalSkillCandidatesHelper
   
   def level_options_hash
-    { t('candidates.skill.beginner_level') => :beginner, 
-      t('candidates.skill.intermediate_level')   => :intermediate, 
-      t('candidates.skill.advanced_level')      => :advanced, 
-      t('candidates.skill.expert_level') => :expert }
+    { t('skills.beginner') => :beginner, 
+      t('skills.intermediate')   => :intermediate, 
+      t('skills.advanced')      => :advanced, 
+      t('skills.expert') => :expert }
   end
   
   def level_options_array
-    [ [ t('candidates.skill.beginner_level'), 'beginner' ],
-      [ t('candidates.skill.intermediate_level'), 'intermediate' ], 
-      [ t('candidates.skill.advanced_level'), 'advanced' ],
-      [ t('candidates.skill.expert_level'), 'expert' ] ]
+    [ [ t('skills.beginner'), 'beginner' ],
+      [ t('skills.intermediate'), 'intermediate' ], 
+      [ t('skills.advanced'), 'advanced' ],
+      [ t('skills.expert'), 'expert' ] ]
   end
   
 end
